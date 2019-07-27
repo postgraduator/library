e4afd18a51d7a6101b2ab3f239acb6508f52dfbd
Vitalii K <smithsonw@outlook.com>
2019-07-27 13:18:04 +0300

    making orders was fixed.

--- library-web/src/main/java/com/nix/libraryweb/model/repository/OrderedBookRepository.java
+++ library-web/src/main/java/com/nix/libraryweb/model/repository/OrderedBookRepository.java
@@ -2,9 +2,11 @@ package com.nix.libraryweb.model.repository;
 
 import java.util.UUID;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.nix.libraryweb.model.entity.OrderedBook;
 
-public interface OrderedBookRepository extends PagingAndSortingRepository<OrderedBook, UUID> {
+@RestResource(exported = false)
+public interface OrderedBookRepository extends CrudRepository<OrderedBook, UUID> {
 }
