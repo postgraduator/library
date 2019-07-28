c1440451749ed0a07c49e82156d9685c64846321
Vitalii K <smithsonw@outlook.com>
2019-07-28 19:01:54 +0300

    pessimistic write for book query was added.

--- library-web/src/main/java/com/nix/libraryweb/model/repository/BookRepository.java
+++ library-web/src/main/java/com/nix/libraryweb/model/repository/BookRepository.java
@@ -2,8 +2,13 @@ package com.nix.libraryweb.model.repository;
 
 import java.util.UUID;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -24,4 +29,8 @@ public interface BookRepository extends PagingAndSortingRepository<Book, UUID> {
     @RestResource(rel = "greater-than", path = "greater-than")
     Page<Book> findBooksByCountGreaterThan(Integer count, Pageable pageable);
 
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(@QueryHint(name = "javax.persistence.lock.timeout", value = "5000"))
+    Iterable<Book> findAllById(Iterable<UUID> uuids);
 }
